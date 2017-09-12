42bcb2a68317872a0a1656f47ae8102250c44d04
Michelle Kidd <michekidd@gmail.com>
2017-09-12 15:40:51 -0500

    instructions

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { View, Dimensions, Text, Image, TouchableHighlight } from 'react-native';
+import Icon from 'react-native-vector-icons';
 import * as Animatable from 'react-native-animatable';
 import Logo from '../assets/images/fotoLingoLogoW.png';
 import Forward from '../assets/images/forward.png';
-import Icon from 'react-native-vector-icons';
+
 
 class Instructions extends Component {
 
@@ -15,30 +16,35 @@ class Instructions extends Component {
 
   render() {
     return (
-<View style={styles.outterContainer}>
+      <View style={styles.outterContainer}>
 
-    <View style={styles.logoBox}>
-      <Animatable.Image
-        source={Logo}
-        animation="pulse" easing="ease-out" iterationCount="infinite"
-        style={styles.logo}
-      />
-    </View>
-    <Animatable.View
-      animation="slideInUp" duration={3000} interationCount={1}
-      style={styles.textBox}
-    >
-      <Text style={styles.header}>INSTRUCTIONS</Text>
-      <Icon name="camera" size={30} color="white" /><Text style={styles.instructions}>Take a photo</Text>
-      <Icon name="list-bullet" size={30} color="white" /><Text style={styles.instructions}>Pick a word</Text>
-      <Icon name="comment-account" size={30} color="white" /><Text style={styles.instructions}>See and hear it in spanish</Text>
-      <Icon name="playlist-check" size={30} color="white" /><Text style={styles.instructions}>See your collected words</Text>
+        <View style={styles.logoBox}>
+          <Animatable.Image
+            source={Logo}
+            animation="pulse" easing="ease-out" iterationCount="infinite"
+            style={styles.logo}
+          />
+        </View>
+        <Animatable.View
+          animation="slideInUp" duration={3000} interationCount={1}
+          style={styles.textBox}
+        >
+          {/* <Text style={styles.header}>
+                INSTRUCTIONS
+              </Text>
+              <Icon name="camera" size={30} color="white" />
+                <Text style={styles.instructions}>
+                  Take a photo
+                </Text>
+          <Icon name="list-bullet" size={30} color="white" /><Text style={styles.instructions}>Pick a word</Text>
+          <Icon name="comment-account" size={30} color="white" /><Text style={styles.instructions}>See and hear it in spanish</Text>
+          <Icon name="playlist-check" size={30} color="white" /><Text style={styles.instructions}>See your collected words</Text> */}
 
-      <TouchableHighlight onPress={() => this.nextPageTakePhoto()}>
-        <Image source={Forward} style={{ height: 40, width: 40 }} />
-      </TouchableHighlight>
-    </Animatable.View>
-  </View>
+          <TouchableHighlight onPress={() => this.nextPageTakePhoto()}>
+            <Image source={Forward} style={{ height: 40, width: 40 }} />
+          </TouchableHighlight>
+        </Animatable.View>
+      </View>
     );
   }
 }
